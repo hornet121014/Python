60993a0d5604f31586bed92fccf3be4829286cbc
AndreiE <125924631+hornet121014@users.noreply.github.com>
2023-09-05 21:00:41 +0300

    Update hw_s4_22_var1.py

--- hw_s4/hw_s4_22_var1.py
+++ hw_s4/hw_s4_22_var1.py
@@ -1,10 +1,15 @@
-def fillset(a,name):
-    myset=set([int(input(f'Введите {i+1} элемент множества {name}: ')) for i in range(a)])
+import os
+os.system('cls')
+
+def fillset(a):
+    myset=set([int(input(f'{i+1}: ')) for i in range(a)])
     return myset
 
 n = int(input('Введите n: '))
 m = int(input('Введите m: '))
-myset1=fillset(n, 'n')
-myset2=fillset(m, 'm')
-mylist=sorted(list(myset1&myset2))
-print(*mylist)
+print('Введите элементы множества n:')
+myset1=fillset(n)
+print('Введите элементы множества m:')
+myset2=fillset(m)
+mylist=sorted(list(myset1 & myset2))
+print(f'Числа, которые встречаются в обоих множествах: {mylist}')
