ace24fc77ca9eabe6df74b9e438171c3af2e4a34
AndreiE <125924631+hornet121014@users.noreply.github.com>
2023-09-25 01:16:50 +0300

    Update cw_s8_49.py in hw_s8_38.py
    insert functional edit and delete contact

--- hw_s8/hw_s8_38.py
+++ hw_s8/hw_s8_38.py
@@ -1,12 +1,6 @@
 """
-Задача №49.
-Создать телефонный справочник с возможностью импорта и экспорта данных в формате .txt. 
-Фамилия, имя, отчество, номер телефона - данные, которые должны находиться в файле. 
-1. Программа должна выводить данные 
-2. Программа должна сохранять данные в текстовом файле 
-3. Пользователь может ввести одну из характеристик для поиска определенной записи(Например имя или фамилию человека) 
-4. Использование функций. 
-Ваша программа не должна быть линейной
+Задача №38.
+Дополнить телефонный справочник возможностью изменения и удаления данных. Пользователь также может ввести имя или фамилию, и Вы должны реализовать функционал для изменения и удаления данных.
 """
 
 import os
@@ -68,24 +62,75 @@ def search_data():
                 print()
 
 
+def edit_data():
+    print('Выбирете вариант изменения:\n'
+          '1. Фамилия\n'
+          '2. Имя\n'
+          '3. Отчество\n'
+          '4. Номер телефона\n'
+          '5. Адрес\n'
+          )
+    index = int(input('→ : '))-1
+    find_str = input('Введите строку поиска: ').title()
+    with open('phonebook.txt', 'r', encoding='UTF-8') as file:
+        data = file.read().strip().split('\n\n')
+        for item in data:
+            new_item = item.replace('\n', ' ').split()
+            if find_str in item:
+                k = data.index(item)
+                new_data = input('Введите новые данные: ').title()
+                new_item[index] = new_data
+                data[k] = f'{new_item[0]} {new_item[1]} {new_item[2]}\n{new_item[3]}\n{new_item[4]}'.strip(
+                )
+                with open('phonebook.txt', 'w', encoding='UTF-8') as file:
+                    file.write('\n\n'.join(data))
+                print('Данные успешно изменены')
+
+
+def delete_data():
+    print('Выбирете вариант удаления:\n'
+          '1. Фамилия\n'
+          '2. Имя\n'
+          '3. Отчество\n'
+          '4. Номер телефона\n'
+          '5. Адрес\n'
+          )
+    index = int(input('→ : '))-1
+    find_str = input('Введите строку поиска: ').title()
+    with open('phonebook.txt', 'r', encoding='UTF-8') as file:
+        data = file.read().strip().split('\n\n')
+        for item in data:
+            new_item = item.replace('\n', ' ').split()
+            if find_str in item:
+                k = data.index(item)
+                data.pop(k)
+                with open('phonebook.txt', 'w', encoding='UTF-8') as file:
+                    file.write('\n\n'.join(data)+'\n\n')
+                print('Данные успешно удалены\n')
+
+
 def interface():
     cmd = '0'
-    while cmd != '4':
+    while cmd != '6':
         print('Выбирете действие:\n'
               '1. добавить контакт\n'
               '2. вывести все контакты\n'
               '3. поиск контакта\n'
-              '4. Выход\n'
+              '4. изменить контакт\n'
+              '5. удалить контакт\n'
+              '6. Выход\n'
               )
         cmd = input('→:')
-        while cmd not in ('1', '2', '3', '4'):
+        while cmd not in ('1', '2', '3', '4', '5', '6'):
             print('Некорректный ввод.')
             cmd = input('→:')
         match cmd:
             case '1': enter_data()
             case '2': print_data()
             case '3': search_data()
-            case '4': print('Всего доброго.')
+            case '4': edit_data()
+            case '5': delete_data()
+            case '6': print('Всего доброго.')
 
 
 interface()
